418e497a37e4d25072ea60acbab6ce5998e3fad7
Sivanand ThiruKumareshan <96750862+sivanand95@users.noreply.github.com>
2022-11-22 16:48:20 +0530

    Update modaltest.java

--- src/com/assignments/modaltest/modaltest.java
+++ src/com/assignments/modaltest/modaltest.java
@@ -55,6 +55,7 @@ public class modaltest {
 			Assert.fail("Test Execution is failed for an exception.");
 			System.out.println("Unable to execute the test! Some Exception is occured! Please check!!");
 			System.out.println(e);
+			System.out.println("MFCS Login message");
 		};
 	}
 	@BeforeTest
